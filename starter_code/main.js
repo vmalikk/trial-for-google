b92b87b82e1833e8c52af0be6e1fa18fd32e1148
vikymalik <vikymalik12034@gmail.com>
2026-01-22 01:30:02 -0500

    sign in

--- starter_code/main.js
+++ starter_code/main.js
@@ -1,8 +1,16 @@
 import "./style.css";
+import { account } from "./appwrite";
+import { OAuthProvider } from "appwrite";
 
 const app = document.getElementById("app");
 const loginBtn = document.getElementById("btn-siwg");
 
-const loginSIWG = async () => {};
+const loginSIWG = async () => {
+    account.createOAuth2Session(
+        OAuthProvider.Google,
+        "https://try.malikv.com/",
+        "https://try.malikv.com/fail"
+    );
+};
 
 loginBtn.addEventListener("click", loginSIWG);
